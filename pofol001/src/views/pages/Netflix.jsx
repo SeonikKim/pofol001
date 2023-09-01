719d01cc05f5ceea75adf3958242b6b7c74b01ce
unknown <tjsdlrdl122@naver.com>
2023-09-02 05:45:19 +0900

    0902

--- pofol001/src/views/pages/Netflix.jsx
+++ pofol001/src/views/pages/Netflix.jsx
@@ -63,27 +63,44 @@ export const Netflix = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='NF-About-Wrap'>
-                        <div className='NF-About'>
-                             <span>5,500원이면 만날 수 있는 넷플릭스.</span>
-                             <span>광고형 스탠다드 멤버십에 가입하세요.</span>
-                             <span>자세히 알아보기 <FontAwesomeIcon icon={faAngleRight} /></span>
+                    <div className='NF-DefWrap'>
+                        <div className='NF-About-Wrap'>
+                            <div className='NF-About'>
+                                <span>5,500원이면 만날 수 있는 넷플릭스.</span>
+                                <span>광고형 스탠다드 멤버십에 가입하세요.</span>
+                                <span>자세히 알아보기 <FontAwesomeIcon icon={faAngleRight} /></span>
+                            </div>
                         </div>
                     </div>
-                    <div className='NF-TV'>
+                    <div className='NF-DefWrap'>
                         <h2>TV로 즐기세요</h2>
                         <span>스마트 TV, PlayStation, Xbox, Chromecast, Apple TV, 블루레이 플레이어 등 다양한 디바이스에서 시청하세요.</span>
                         <div className='TV-Wrap'>
-                            
+
                             <img
-                            alt=""
-                            src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
-                             data-uia="nmhp-card-animation-asset-image"
-                              class="NF-TV-Img"/>
-                              <div className='NF-TV-Container'>
-                              <VideoPlayer/>
-                              </div>
+                                alt=""
+                                src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/tv.png"
+                                data-uia="nmhp-card-animation-asset-image"
+                                class="NF-TV-Img" />
+                            <div className='NF-TV-Container'>
+                                <VideoPlayer />
+                            </div>
                         </div>
+                    </div>
+                    <div className='NF-DefWrap'>
+                        <h2>어디서나 자유롭게 시청하세요</h2>
+                        <span>각종 영화와 시리즈를 스마트폰, 태블릿, 노트북, TV에서 무제한으로 스트리밍하세요.</span>
+                    </div>
+
+                    <div className='NF-DefWrap'>
+                        <h2>어린이 전용 프로필을 만들어 보세요</h2>
+                        <span>자기만의 공간에서 좋아하는 캐릭터와 즐기는 신나는 모험. 자녀에게 이 특별한 경험을 선물하세요. 넷플릭스 회원이라면 무료입니다.</span>
+
+                    </div>
+                    <div className='NF-DefWrap'>
+                        <h2>즐겨 보는 콘텐츠를 저장해 오프라인으로 시청하세요</h2>
+                        <span>광고 없는 멤버십에서만 이용 가능합니다.</span>
+
                     </div>
                 </div>
             </div>
